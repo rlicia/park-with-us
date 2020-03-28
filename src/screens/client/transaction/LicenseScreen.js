85d2ecef541ee2fec735ecf7d0fa2b984f0d34b2
rlicia <thanakit.limakun@hotmail.com>
2020-03-28 19:33:10 +0700

    test flow

--- src/screens/client/transaction/LicenseScreen.js
+++ src/screens/client/transaction/LicenseScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-const LicenseScreen = () => {
+const LicenseScreen = ({ navigation }) => {
     return (
         <View>
             <Text>License Screen</Text>
+            <Button title="Click" onPress={() => navigation.navigate('Zone')} />
         </View>
     );
 };
