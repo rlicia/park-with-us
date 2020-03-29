b8c738f2e284c92572157cda645f405b4025d4f8
rlicia <thanakit.limakun@hotmail.com>
2020-03-29 09:56:55 +0700

    Edit

--- src/screens/user/tier/TierHomeScreen.js
+++ src/screens/user/tier/TierHomeScreen.js
@@ -1,11 +1,63 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, ScrollView, TouchableHighlight } from 'react-native';
+
+import { Context as AuthContext } from '../../../contexts/AuthContext';
+
+import Header from '../../../components/Header';
+
+const ButtonForm = ({ title, onSubmit }) => {
+    return (
+        <TouchableHighlight
+            style={button.container}
+            underlayColor='#00000030'
+            onPress={onSubmit}
+        >
+            <View style={button.button}>
+                <Text style={button.title}>{title}</Text>
+            </View>
+        </TouchableHighlight>
+    );
+};
+
+const button = StyleSheet.create({
+    container: {
+        height: 80,
+        paddingHorizontal: 15
+    },
+    button: {
+        flex: 1,
+        paddingVertical: 20,
+        borderBottomWidth: 1,
+        borderColor: '#D4D4D4',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 18
+    }
+});
 
 const TierHomeScreen = () => {
+    const { navigateTo } = useContext(AuthContext);
+
     return (
-        <View>
-            <Text>TierHome Screen</Text>
-        </View>
+        <Header
+            title='Tier'
+            userScreen={true}
+            backButton='UserHome'
+        >
+            <ScrollView>
+                <ButtonForm 
+                    title='Client'
+                    onSubmit={() => navigateTo('TierList', { status: 1 })}
+                />
+                <ButtonForm
+                    title='User'
+                    onSubmit={() => navigateTo('TierList', { status: 0 })}
+                />
+            </ScrollView>
+        </Header>
     );
 };
 
