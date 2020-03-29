fea5b3d7ff5710037a72a381e8b5aab4d361e3fd
rlicia <thanakit.limakun@hotmail.com>
2020-03-29 11:30:10 +0700

    Edit

--- src/screens/user/account/AccountHomeScreen.js
+++ src/screens/user/account/AccountHomeScreen.js
@@ -1,11 +1,67 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
+import { NavigationEvents } from '@react-navigation/compat';
+
+import { Context as AuthContext } from '../../../contexts/AuthContext';
+import { Context as AccountContext } from '../../../contexts/AccountContext';
+
+import Header from '../../../components/Header';
+
+const ButtonForm = ({ title, icon, onSubmit }) => {
+    return (
+        <TouchableHighlight
+            style={button.container}
+            underlayColor='#00000030'
+            onPress={onSubmit}
+        >
+            <View style={button.button}>
+                <Text style={button.title}>{title}</Text>
+            </View>
+        </TouchableHighlight>
+    );
+};
+
+const button = StyleSheet.create({
+    container: {
+        height: 80,
+        paddingHorizontal: 15
+    },
+    button: {
+        flex: 1,
+        paddingVertical: 20,
+        borderBottomWidth: 1,
+        borderColor: '#D4D4D4',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 18
+    }
+});
 
 const AccountHomeScreen = () => {
+    const { navigateTo } = useContext(AuthContext);
+    const { clearAccountData } = useContext(AccountContext);
+
     return (
-        <View>
-            <Text>AccountHome Screen</Text>
-        </View>
+        <Header
+            title='Client or User'
+            userScreen={true}
+            backButton='UserHome'
+        >
+            <NavigationEvents
+                onWillFocus={clearAccountData}
+            />
+            <ButtonForm 
+                title='Client'
+                onSubmit={() => navigateTo('AccountList', { status: 1 })}
+            />
+            <ButtonForm 
+                title='User'
+                onSubmit={() => navigateTo('AccountList', { status: 0 })}
+            />
+        </Header>
     );
 };
 
