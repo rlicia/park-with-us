1cb700b3657207f7c50f4c017f30dc8255f4014b
rlicia <thanakit.limakun@hotmail.com>
2020-03-29 11:03:37 +0700

    Edit loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,40 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet, Modal, ActivityIndicator } from 'react-native';
+import { StyleSheet, ActivityIndicator } from 'react-native';
 
-const Loader = ({ title, loading }) => {
+const Loader = ({ loading }) => {
     return (
-        <Modal
-            transparent={true}
-            animationType='none'
-            visible={loading}
-        >
-            <View style={styles.modalBackground}>
-                <View style={styles.activityIndivatorWrapper}>
-                    <ActivityIndicator animating={loading} color='white' />
-                    <Text style={styles.title}>{title}</Text>
-                </View>
-            </View>
-        </Modal>
+        <ActivityIndicator
+            style={styles.activityIndicator}
+            animating={loading ? true: false}
+            color='black'
+            size='large'
+        />
     );
 };
 
 const styles = StyleSheet.create({
-    modalBackground: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#00000050'
-    },
-    activityIndivatorWrapper: {
-        backgroundColor: '#00000099',
-        height: 100,
-        width: 200,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        borderRadius: 20
-    },
-    title: {
-        color: 'white'
+    activityIndicator: {
+        position: 'absolute',
+        ...StyleSheet.absoluteFillObject
     }
 });
 
