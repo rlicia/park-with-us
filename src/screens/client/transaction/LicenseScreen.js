bfaadaf5ed5ef46dc8ea6fff3b67dbd6608bf871
rlicia <thanakit.limakun@hotmail.com>
2020-03-29 15:17:57 +0700

    edit

--- src/screens/client/transaction/LicenseScreen.js
+++ src/screens/client/transaction/LicenseScreen.js
@@ -53,7 +53,6 @@ const LicenseScreen = () => {
             title='License'
         >
             <NavigationEvents
-                onWillFocus={() => initialLoadTransaction()}
                 onWillBlur={() => {
                     setLicenseTitle('');
                     setLicenseNumber('');
