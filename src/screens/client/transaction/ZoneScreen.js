58be46f6de029bda400183649bbff02d21d36575
rlicia <thanakit.limakun@hotmail.com>
2020-03-29 13:29:35 +0700

    edit

--- src/screens/client/transaction/ZoneScreen.js
+++ src/screens/client/transaction/ZoneScreen.js
@@ -1,16 +1,142 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, TouchableHighlight, TouchableOpacity } from 'react-native';
+import { NavigationEvents } from '@react-navigation/compat';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import { Context as TransactionContext } from '../../../contexts/TransactionContext';
+import { Context as AuthContext } from '../../../contexts/AuthContext';
+
+import Header from '../../../components/Header';
+import Loader from '../../../components/Loader';
 
 const ZoneScreen = () => {
+    const { state: authState, navigateTo } = useContext(AuthContext);
+    const { state: transactionState, fetchZones, clearZoneList } = useContext(TransactionContext);
+
     return (
-        <View>
-            <Text>Zone Screen</Text>
-        </View>
+        <Header
+            title='Select Zone'
+            backButton='License'
+            headerRight={
+                <TouchableOpacity
+                    onPress={fetchZones}
+                >
+                    <Icon name='refresh' size={24} />
+                </TouchableOpacity>
+            }
+        >
+            <NavigationEvents
+                onWillFocus={() => fetchZones()}
+                onWillBlur={clearZoneList}
+            />
+            <Loader
+                title={transactionState.loading}
+                loading={transactionState.loading ? true : false}
+            />
+            <View style={styles.topContainer}>
+                <View style={styles.licenseContainer}>
+                    <Text style={styles.licenseTitle}>License:</Text>
+                    <Text style={styles.license}>{transactionState.license}</Text>
+                </View>
+                <View style={styles.detailContainer}>
+                    <Text style={styles.detailTitle}>Tier:</Text>
+                    <Text style={styles.detail}>{authState.account.tier}</Text>
+                </View>
+            </View>
+            <FlatList
+                data={transactionState.zoneList}
+                keyExtractor={item => item._id}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableHighlight
+                            style={styles.listItemContainer}
+                            underlayColor='#00000030'
+                            onPress={() => navigateTo('Slot', { zone: item })}
+                        >
+                            <View style={styles.listItem}>
+                                <View style={styles.listTitleContainer}>
+                                    <Text style={styles.listTitle}>
+                                        {item.tierName}
+                                    </Text>
+                                </View>
+                                <View style={styles.availableContainer}>
+                                    <Text style={styles.availableTitle}>
+                                        Available:
+                                    </Text>
+                                    <Text style={styles.slotAvailable}>
+                                        {item.slotAvailable}
+                                    </Text>
+                                </View>
+                            </View>
+                        </TouchableHighlight>
+                    );
+                }}
+            />
+        </Header>
     );
 };
 
 const styles = StyleSheet.create({
-
+    topContainer: {
+        borderBottomWidth: 1,
+        paddingVertical: 10,
+        paddingHorizontal: 5,
+        marginHorizontal: 15
+    },
+    licenseContainer: {
+        flexDirection: 'row',
+        paddingBottom: 5
+    },
+    licenseTitle: {
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    license: {
+        fontSize: 18,
+        paddingLeft: 5
+    },
+    detailContainer: {
+        flexDirection: 'row'
+    },
+    detailTitle: {
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    detail: {
+        fontSize: 18,
+        paddingLeft: 5
+    },
+    listItemContainer: {
+        height: 80
+    },
+    listItem: {
+        flex: 1,
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderBottomColor: '#D4D4D4',
+        marginHorizontal: 15,
+        alignItems: 'center'
+    },
+    listTitleContainer: {
+        flex: 7,
+        paddingLeft: 10
+    },
+    listTitle: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    availableContainer: {
+        flex: 3,
+        flexDirection: 'row'
+    },
+    availableTitle: {
+        fontWeight: 'bold'
+    },
+    slotAvailable: {
+        fontWeight: 'bold',
+        color: '#00AB66',
+        paddingLeft: 5
+    }
 });
 
 export default ZoneScreen;
